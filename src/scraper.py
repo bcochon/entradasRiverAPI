728441f2aedc663a834f88555237c7c3675c9f4a
bcochon <brunoxosec@gmail.com>
2025-02-11 04:01:14 -0300

    Noticias se relacionan con partidos

--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-from utils import merge_lists
+from datetime import datetime
+from utils import merge_lists, get_date, format_date
 
 RIVER_URL = 'https://www.cariverplate.com.ar/'
-cache_noticias = []
-cache_partidos = []
 
 class Entradas:
     def __init__(self, noticia):
@@ -12,30 +11,44 @@ class Entradas:
         self.date = self.find_date(noticia.url)
 
     def find_date(self, url):
-        ...
+        return None
 
 class Partido:
     def __init__(self, vs: str, torneo: str, date: str, entradas: Entradas = None):
         self.vs = vs
         self.torneo = torneo
-        self.date = date
+        self.date = format_date(date)
         self.entradas = entradas
+
+    def __str__(self):
+        return f'{self.date.strftime('%c')} - {self.vs} ({self.torneo})'
+
+    def coincide(self, title: str, date: datetime):
+        return self.vs.lower() in title.lower() and self.date.day == date.day and self.date.month == date.month
 
 class Noticia:
     def __init__(self, title: str, description: str, url: str):
         self.title = title
         self.description = description
         self.url = url
-        self.partido = self.fetch_partido(title, description)
+        self.partido = self.fetch_partido()
 
     def __str__(self):
-        return f'{self.title}\n{self.url}\n{self.description}'
+        return f'{self.title}\n{self.url}\n{self.description}\n{self.partido}'
     
     def fetch_partido(self):
         if 'venta de entradas' not in self.title.lower():
             return None
         self.entradas = Entradas(self)
+        partidos = get_partidos()
+        for partido in partidos:
+            if partido.coincide(self.title, get_date(self.description)):
+                return partido
         return None
+
+
+cache_noticias = []
+cache_partidos: list[Partido] = []
 
 
 def build_noticias(soups: list[BeautifulSoup]) -> list[Noticia]:
@@ -60,13 +73,14 @@ def build_partidos(soups: list[BeautifulSoup]) -> list[Partido]:
 def scrap_noticias():
     info = requests.get(RIVER_URL+'noticias-de-entradas')
     soup = BeautifulSoup(info.content, 'html.parser')
+    global cache_noticias
     try:
         soup_rows = soup.find('section', id='principal').find_all('div', class_='row')[1:]
         first_row = soup_rows[0].find_all('figure')
-        if first_row[0] == cache_noticias[0]:
+        if len(cache_noticias) and first_row[0] == cache_noticias[0]:
             return cache_noticias
         second_row_columns = soup_rows[1].find_all('div', class_='col-lg-6 col-md-6 col-xs-12')
-        second_row = merge_lists(second_row_columns[0].find_all('figure'), second_row_columns[1].find_all('figure'))
+        second_row = merge_lists(second_row_columns[0].find_all('figure'), second_row_columns[1].find_all('figure')) 
         cache_noticias = first_row + second_row
     except Exception as e:
         print(e)
@@ -76,10 +90,9 @@ def scrap_partidos():
     info = requests.get(RIVER_URL+'calendario-de-partidos')
     soup = BeautifulSoup(info.content, 'html.parser')
     try:
-        cache_partidos = soup.find('div', id='caledario').find('div', class_='calendario').find_all('li')
+        return soup.find('div', id='caledario').find('div', class_='calendario').find_all('li')
     except Exception as e:
         print(e)
-    return cache_partidos
 
 def get_noticias(number: int = 10) -> list[Noticia]:
     noticias = scrap_noticias()
@@ -87,8 +100,11 @@ def get_noticias(number: int = 10) -> list[Noticia]:
 
 def get_partidos(number: int = 30) -> list[Partido]:
     partidos = scrap_partidos()
-    return build_partidos(partidos[:number])
+    global cache_partidos
+    cache_partidos = build_partidos(partidos[:number])
+    return cache_partidos
 
 if __name__ == '__main__':
-    # [print(noticia, '\n') for noticia in get_noticias(10)]
-    get_partidos()
+    noticias = get_noticias(10)
+    for noticia in noticias:
+        print(noticia)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,37 @@
-def find_date(input: str) -> str:
-    ...
+import re
+from datetime import datetime
 
-def format_date(input: str) -> str:
-    ...
+MESES = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+MESES_REGEX = 'enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre'
+
+def get_date(input: str) -> datetime:
+    match = re.search(f"[1-3]?[0-9] de ({MESES_REGEX})", input.lower())
+    if match:
+        return format_date(match.group(), ddmmyyyy=False)
+    return None
+
+def format_date(input: str, ddmmyyyy = True) -> datetime:
+    if ddmmyyyy:
+        match = re.search("[0-3]?[0-9]/[0-1]?[0-9]/[0-9]{1,4}", input)
+        if match:
+            day, month, year = list(map(lambda s: int(s), match.group().split('/')))
+            date = datetime(year, month, day)
+            hourMatch = re.search("[0-2]?[0-9]([.:-][0-5][0-9])?$", input.replace(match.group(),''))
+            if hourMatch:
+                time = list(map(lambda s: int(s), hourMatch.group().replace(':','.').replace('-','.').split('.')))
+                if(len(time) > 1):
+                    date = datetime(year, month, day, time[0], time[1])
+                else:
+                    date = datetime(year, month, day, time[0])
+            return date
+    day_month = input.replace(' ','').split('de')
+    if(len(day_month) < 2):
+        return None
+    day = int(day_month[0])
+    month = MESES.index(day_month[1])+1
+    year = datetime.now().year
+    date = datetime(year, month, day)
+    return date
 
 def merge_lists(a: list, b: list) -> list:
     result = []
@@ -16,4 +45,5 @@ def merge_lists(a: list, b: list) -> list:
     return result + a[i:]
 
 if __name__ == '__main__':
-    ...
+    text = 'Domingo 02/02/2025 - 17.55'
+    print(format_date(text))
